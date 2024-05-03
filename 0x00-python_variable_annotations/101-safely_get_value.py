39f2e81d807075d6a18e15b6829c62375c373d93
colly-m <sindanicolly@gmail.com>
2024-05-03 21:17:26 +0000

    Mass involved type annotated

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module to add type annotations to functions"""
+from typing import Mapping, TypeVar, Union, Any
+
+
+T = TypeVar('T')
+V = Union[Any, T]
+D = Union[T, None]
+
+def safely_get_value(dct: Mapping, key: Any, default: D = None) -> V:
+    """Function to get the value from a dictionary using the key"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
+
