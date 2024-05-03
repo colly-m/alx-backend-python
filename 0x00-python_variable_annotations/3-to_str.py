77dd7d1bde4928d5cf76ccf11e44fd19c9a55992
colly-m <sindanicolly@gmail.com>
2024-05-03 13:57:04 +0000

    Basic to string update

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module to take a float and return a string representation of it"""
+
+
+def to_str(n: float) -> str:
+    """Function that returns the string equivalent of any float argument"""
+    return str(n)
