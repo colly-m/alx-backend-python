99985d3ed35d8fcbec2f2ab5ae22e183b2099281
colly-m <sindanicolly@gmail.com>
2024-05-03 12:33:40 +0000

    Basic concatenation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module to concatenate two strings into one and returns it"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function to concatenate str1 and str2"""
+    return str1 + str2
