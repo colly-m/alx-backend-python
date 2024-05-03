ae88c498bdf0328a69bf570bdfce9453417c983f
colly-m <sindanicolly@gmail.com>
2024-05-03 15:29:20 +0000

    Mixed complex list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module to take list of ints and floats to return sum as float"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[float]) -> float:
+    """Function to take a listof ints and floats and return float sum"""
+    return sum(mxd_lst)
