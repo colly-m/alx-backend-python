20ebf635a8f631f7c22b1651e1d5334ba7e484d8
colly-m <sindanicolly@gmail.com>
2024-05-03 14:14:36 +0000

    Float complexity types

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module to take sum_list a list of input_list floats as args
+and returns float sum"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Function to sum up floats and return a float"""
+    return sum(input_list)
