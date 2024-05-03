29616cb558d0aeb305be3387a7fb09cddc6cb235
colly-m <sindanicolly@gmail.com>
2024-05-03 14:01:04 +0000

    Variables defined

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module to annotate variables"""
+
+
+a = 1  # Integer with a value of 1
+pi = 3.14  # Float with a value of 3.14
+i_understand_annotations = True  # Boolean with a value of True
+school = "Holberton"  # String with a value of "Holberton"
