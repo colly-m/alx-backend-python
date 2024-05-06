408b7daa370e415d31d871fb281de58f0897fc64
colly-m <sindanicolly@gmail.com>
2024-05-06 23:13:31 +0000

    Tasked up update

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module to create an async function using function syntax"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+def task_wait_random(max_delay):
+    """Function to create a task for the scheduler"""
+    loop = asyncio.get_event_loop()
+    return loop.create_task(wait_random(max_delay))
