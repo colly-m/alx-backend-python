c24d33bd96dbb5d49a7b1fedc1ce529f95d17ca7
colly-m <sindanicolly@gmail.com>
2024-05-06 23:19:28 +0000

    Task up update

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,9 +5,11 @@ import random
 
 
 async def wait_random(max_delay=10):
+    """Function to return variables with a delay"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
+
 
 def task_wait_random(max_delay):
     """Function to create a task for the scheduler"""
