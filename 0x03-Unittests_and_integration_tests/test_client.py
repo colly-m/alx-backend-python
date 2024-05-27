bfde1d7c5a534db48ebf57d536cc04cef6777afb
colly-m <sindanicolly@gmail.com>
2024-05-27 23:54:14 +0000

    Public repos rest update

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -14,7 +14,6 @@ class TestGithubOrgClient(unittest.TestCase):
         ("abc",),
     ])
     @patch('client.get_json', return_value={"login": "mocked_org"})
-
     def test_org(self, org_name, mock_get_json):
         """Function to test GithubOrgClient.org returning the correct value"""
         client = GithubOrgClient(org_name)
@@ -25,11 +24,30 @@ class TestGithubOrgClient(unittest.TestCase):
 
         self.assertEqual(result, {"login": "mocked_org"})
 
-    def test_public_repos_url(self):
-        """Function to test GithubOrgClient._public_repos_url returns the expected value"""
-        with patch.object(GithubOrgClient, 'org', new_callable=PropertyMock) as mock_org:
-            mock_org.return_value = {'repos_url': 'https://api.github.com/orgs/google/repos'}
-            client = GithubOrgClient('google')
-            result = client._public_repos_url
+    def test_public_repos_url(self, name, outcome):
+        """Function to test GithubOrgClient._public_repos_url"""
+        with patch('client.GithubOrgClient.org',
+                   PropertyMock(return_value=outcome)):
+            response = GithubOrgClient(name)._public_repos_url
+            self.assertEqual(response, outcome.get('repos_url'))
 
-            self.assertEqual(result, 'https://api.github.com/orgs/google/repos')
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """Function to test in github for public_repos return repo list"""
+        mock_get_json.return_value = [
+            {'name': 'repo1'},
+            {'name': 'repo2'}
+        ]
+
+        with patch.object(GithubOrgClient, '_public_repos_url',
+                          new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value =
+            'https://api.github.com/orgs/google/repos'
+            client = GithubOrgClient('google')
+            result = client.public_repos()
+
+            self.assertEqual(result, [{'name': 'repo1'}, {'name': 'repo2'}])
+            mock_public_repos_url.assert_called_once()
+
+            mock_get_json.assert_called_once_with(
+                    'https://api.github.com/orgs/google/repos')
